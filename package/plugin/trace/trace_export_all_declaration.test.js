65c6e30c4a691d7a01ebbca4660a8d28123f3588
gtsop <gtsop@protonmail.com>
2024-05-04 03:23:14 +0300

    update test case for namespace export

--- package/plugin/trace/trace_export_all_declaration.test.js
+++ package/plugin/trace/trace_export_all_declaration.test.js
@@ -1,45 +1,39 @@
+/*
+ * List of syntaxes to be tested are sourced from MDN
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export#syntax
+ */
 const { trace_export_all_declaration } = require('./trace_export_all_declaration');
 const { babelParse } = require('../utils');
 const babelTraverse = require('@babel/traverse').default;
-
-let state = {};
 
 function traverse(code, visitors) {
   const ast = babelParse(code);
   return babelTraverse(ast, visitors);
 }
 
+function expectState(code, specifier, expectedState) {
+  const state = { match: false, traces: [] };
+  const visitor = trace_export_all_declaration(state, specifier, './source.js', (p) => p)
+  traverse(code, { ...visitor });
+  expect(state.match).toEqual(expectedState.match);
+  expect(state.traces).toEqual(expectedState.traces);
+}
+
+function expectTrace(code, specifier, traceName = specifier) {
+  expectState(code, specifier, {
+    match: false,
+    traces: [{
+      name: traceName,
+      source: './origin.js',
+      file: './source.js'
+    }],
+  })
+}
+
 describe('trace/trace_export_all_declaration', function() {
 
-  beforeEach(function() {
-    state = new Object({ match: false, traces: new Array() });
-  });
-
-  it('does not return a visitor when the specifier is "default"', function() {
-    expect(trace_export_all_declaration(state, 'default', 'source.js')).toEqual({});
-  });
-
-  it('returns an "ExportAllDeclaration" visitor when specifier is not "default"', function() {
-
-    const visitor = trace_export_all_declaration(state, 'specifier', 'source.js')
-
-    expect(visitor.ExportAllDeclaration).toBeInstanceOf(Function);
-  });
-
-
-  describe('export * from "./original"', () => {
-    it('traces specifier to glob exports', function() {
-
-      const visitor = trace_export_all_declaration(state, 'specifier', 'source.js', (p) => p);
-
-      traverse('export * from "./original"', { ...visitor });
-
-      expect(state.match).toEqual(false);
-      expect(state.traces).toEqual([{
-        name: 'specifier',
-        source: './original',
-        file: 'source.js'
-      }])
-    });
-  });
+  it("traces namespace exports", () => {
+    expectTrace("export * from './origin.js';", "specifier")
+  })
 });
