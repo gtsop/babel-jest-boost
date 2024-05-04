32b4f39b90d930841041a48323c3cdf596b298a3
george.tsopanoglou <george.tsopanoglou@dialecticanet.com>
2024-05-05 00:01:49 +0300

    add eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintPluginJest from "eslint-plugin-jest";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   {
     plugins: {
       eslintPluginJest,
+      eslintConfigPrettier,
     },
   },
   {
